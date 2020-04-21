3d4ba2ee5bfbc34aabc6508a3cc289e44a3fd7a3
Kun-Yi Li <LanKuDot@users.noreply.github.com>
2020-04-21 16:52:26 +0800

    Update ml_play.py

--- ml_play.py
+++ ml_play.py
@@ -1,42 +1,39 @@
-import games.arkanoid.communication as comm
-from games.arkanoid.communication import SceneInfo, PlatformAction, GameStatus
+from mlgame.communication import ml as comm
 import pickle
 from sklearn.ensemble import RandomForestRegressor
 import os.path
 
 def ml_loop():
-    
     filename = 'ian.sav'
     filepath = os.path.join(os.path.dirname(__file__), filename)
     model = pickle.load(open(filepath, 'rb'))
     comm.ml_ready()
+    scene_info = comm.recv_from_game()
+    comm.send_to_game({"frame": scene_info["frame"], "command": "SERVE_TO_LEFT"})
 
     ballspeedx = 0
     ballspeedy = 0
     ballx = 102.5
     bally = 102.5
     predictx = 100
-    
-    scene_info = comm.get_scene_info()
-    comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_LEFT)
-    
-    while True:
-        scene_info = comm.get_scene_info()
-        
-        if scene_info.status == GameStatus.GAME_OVER:
-            scene_info = comm.get_scene_info()
-            break
-        elif scene_info.status == GameStatus.GAME_PASS:
-            scene_info = comm.get_scene_info()
 
-        ballspeedx = scene_info.ball[0] + 2.5 - ballx
-        ballspeedy = scene_info.ball[1] + 2.5 - bally
-        ballx = scene_info.ball[0] + 2.5
-        bally = scene_info.ball[1] + 2.5
-        platformx = scene_info.platform[0] + 20
+    while True:
+        scene_info = comm.recv_from_game()
+
+        if scene_info["status"] == "GAME_OVER":
+            scene_info = comm.recv_from_game()
+            break
+        elif scene_info["status"] == "GAME_PASS":
+            scene_info = comm.recv_from_game()
+
+        ballspeedx = scene_info["ball"][0] + 2.5 - ballx
+        ballspeedy = scene_info["ball"][1] + 2.5 - bally
+        ballx = scene_info["ball"][0] + 2.5
+        bally = scene_info["ball"][1] + 2.5
+        platformx = scene_info["platform"][0] + 20
         predictx = model.predict([[ballspeedx, ballspeedy, ballx, bally]])[0]
 
         if predictx - 3 > platformx:
-            comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
+            comm.send_to_game({"frame": scene_info["frame"], "command": "MOVE_RIGHT"})
         elif predictx + 3 < platformx:
-            comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+            comm.send_to_game({"frame": scene_info["frame"], "command": "MOVE_LEFT"})
