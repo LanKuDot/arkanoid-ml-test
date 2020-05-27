9aab908a72cf740048414fa2730199b6b2fbb46a
Kun-Yi Li <LanKuDot@users.noreply.github.com>
2020-05-27 16:28:33 +0800

    Update ml_play.py

--- ml_play.py
+++ ml_play.py
@@ -1,39 +1,36 @@
-from mlgame.communication import ml as comm
 import pickle
 from sklearn.ensemble import RandomForestRegressor
 import os.path
 
-def ml_loop():
-    filename = 'ian.sav'
-    filepath = os.path.join(os.path.dirname(__file__), filename)
-    model = pickle.load(open(filepath, 'rb'))
-    comm.ml_ready()
-    scene_info = comm.recv_from_game()
-    comm.send_to_game({"frame": scene_info["frame"], "command": "SERVE_TO_LEFT"})
+class MLPlay:
+    def __init__(self):
+        filename = 'ian.sav'
+        filepath = os.path.join(os.path.dirname(__file__), filename)
+        self.model = pickle.load(open(filepath, 'rb'))
+        self.ballx = 102.5
+        self.bally = 102.5
+        self.ball_served = False
 
-    ballspeedx = 0
-    ballspeedy = 0
-    ballx = 102.5
-    bally = 102.5
-    predictx = 100
+    def update(self, scene_info):
+        if (scene_info["status"] == "GAME_OVER" or
+            scene_info["status"] == "GAME_PASS"):
+            return "RESET"
 
-    while True:
-        scene_info = comm.recv_from_game()
-
-        if scene_info["status"] == "GAME_OVER":
-            scene_info = comm.recv_from_game()
-            break
-        elif scene_info["status"] == "GAME_PASS":
-            scene_info = comm.recv_from_game()
-
-        ballspeedx = scene_info["ball"][0] + 2.5 - ballx
-        ballspeedy = scene_info["ball"][1] + 2.5 - bally
-        ballx = scene_info["ball"][0] + 2.5
-        bally = scene_info["ball"][1] + 2.5
+        ballspeedx = scene_info["ball"][0] + 2.5 - self.ballx
+        ballspeedy = scene_info["ball"][1] + 2.5 - self.bally
+        self.ballx = scene_info["ball"][0] + 2.5
+        self.bally = scene_info["ball"][1] + 2.5
         platformx = scene_info["platform"][0] + 20
-        predictx = model.predict([[ballspeedx, ballspeedy, ballx, bally]])[0]
+        predictx = self.model.predict(
+            [[ballspeedx, ballspeedy, self.ballx, self.bally]])[0]
 
+        if not self.ball_served:
+            self.ball_served = True
+            return "SERVE_TO_LEFT"
         if predictx - 3 > platformx:
-            comm.send_to_game({"frame": scene_info["frame"], "command": "MOVE_RIGHT"})
-        elif predictx + 3 < platformx:
-            comm.send_to_game({"frame": scene_info["frame"], "command": "MOVE_LEFT"})
+            return "MOVE_RIGHT"
+        if predictx + 3 < platformx:
+            return "MOVE_LEFT"
+
+    def reset(self):
+        self.ball_served = False
